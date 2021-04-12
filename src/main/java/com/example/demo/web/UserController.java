0aab17e91d090b4ed6760d95d46a41248cc7be53
yijing.lv <yijing.lv@shopee.com>
2021-04-12 16:17:10 +0800

    user with version1

--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -2,10 +2,7 @@ package com.example.demo.web;
 
 import com.example.demo.dao.User;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +13,33 @@ public class UserController {
 //    static 关键字表示是类变量
     static ArrayList<User> users = new ArrayList<User>();
     static int id;
-//    需要有构造器吗？
+//    需要有构造器吗？- 不需要
     @GetMapping
     public ArrayList<User> getUsers(){
         return users;
     }
+    //    前端如何传参访问： curl --location --request PUT 'http://localhost:8080/user?name=yijing2&age=20'
     @PutMapping
     public String insertUser(String name, int age){
         User user = new User();
-        user.setId(UserController.id);
+        user.setId(id);
         user.setName(name);
         user.setAge(age);
         users.add(user);
+        id += 1;
         for(User u : users){
             System.out.println(u);
         }
         return "ok";
+    }
+    @DeleteMapping
+    public User deleteUser(String name){
+        for (User u : users){
+            if (u.getName().equals(name)){
+                users.remove(u);
+                return u;
+            }
+        }
+        return null;
     }
 }
